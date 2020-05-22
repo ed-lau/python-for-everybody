7d22a88817ad58fd119eea0cbacaa7929a42d4a8
Devansh Tayal <58529382+Devansh2005@users.noreply.github.com>
2020-05-22 19:31:26 +0530

    Update wk 10 - assignment 10.2.py

--- wk 10 - assignment 10.2.py
+++ wk 10 - assignment 10.2.py
@@ -10,27 +10,26 @@ sorted by hour as shown below. Note that the autograder
 does not have support for the sorted() function.
 """
 
+
+
 name = raw_input("Enter file:")
 if len(name) < 1 : name = "mbox-short.txt"
-text = open(name)
+handle = open(name)
 
-hours = dict()
+hcount = dict()                                     #create empty dictionary
+hlst = []                                           #create empty list
 
-for line in text:
-    line.rstrip()
-    if not line.startswith("From "): continue
+for line in handle: 
     words = line.split()
+    if len(words) > 2 and words[0] == 'From':       #Select lines with 'From'
+        hr = words[5].split(':')                    #Select hour (5th index) and split string with colon
+        hcount[hr[0]] = hcount.get(hr[0], 0) + 1    #increase count for each hour
+    else:
+        continue
 
-    hour = words[5].split(":")
-    hours[hour[0]] = hours.get(hour[0],0) + 1
+for k,v in hcount.items():                           #k = hour, v = count
+    hlst.append((k,v))                               #append tuples to list
 
-lst = []
-
-for a,b in hours.items():
-    lst.append((a,b))
-
-lst.sort()
-
-
-for a,b in lst:
-    print a,b
+hlst.sort()                                         #sort list by hour
+for k,v in hlst:                                    #loop through list of tuples
+    print k,v                                       #print counts sorted by hour
