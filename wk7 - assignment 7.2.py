cebd047d212ede6db244a82726a1452e70cb0e92
Mridul <66078628+pmmridul@users.noreply.github.com>
2021-04-04 12:41:25 +0530

    Update wk7 - assignment 7.2.py
    Made some changes to the solution program.
    In the earlier program, last print line didn't have parenthesis.
    Within the for loop, else condition wasn't specified.

--- wk7 - assignment 7.2.py
+++ wk7 - assignment 7.2.py
@@ -7,15 +7,17 @@ you are testing below enter mbox-short.txt as the file name.
 
 
 # Use the file name mbox-short.txt as the file name
-fname = raw_input("Enter file name: ")
+fname = input("Enter file name: ")
 fh = open(fname)
 sum = 0.0
 count = 0
 
 for line in fh:
-        if not line.startswith("X-DSPAM-Confidence:") : continue
-sum = sum + float(line[20:])
-count = count + 1
+        if not line.startswith("X-DSPAM-Confidence:") :
+                continue
+        else:
+                sum = sum + float(line[20:])
+                count = count + 1
 
-print "Average spam confidence:", sum/count
+print("Average spam confidence:", sum/count)
 
