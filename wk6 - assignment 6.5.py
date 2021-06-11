893482a12b6b75e6a6d1a2142357b81120bd1bf3
synbiomotif <85665565+synbiomotif@users.noreply.github.com>
2021-06-10 22:43:09 -0700

    Update wk6 - assignment 6.5.py

--- wk6 - assignment 6.5.py
+++ wk6 - assignment 6.5.py
@@ -1,11 +1,8 @@
 #6.5 Write code using find() and string slicing (see section 6.10) to extract the number at the end of the line below.
 #Convert the extracted value to a floating point number and print it out.
 
-text = "X-DSPAM-Confidence:    0.8475";
-str = 'X-DSPAM-Confidence: 0.8475'
-ipos = str.find(':')
-#print(ipos)
-piece = str[ipos+2:]
-#print(piece)
-value = float(piece)
-print(value)
+text = "X-DSPAM-Confidence:    0.8475"
+start = text.find('0')
+result =float(text[start : ])
+print (result)
+
